ba6641ac4968d10d25bad496d2ea5261d83e3759
Pietro <pietro.bambagioni@stud.unifi.it>
2025-01-09 15:45:27 +0100

    main ready

--- main.py
+++ main.py
@@ -2,7 +2,49 @@ from codice import Mappa as Mappa, Config as Config, Drone as Drone
 import json,os
 from datetime import datetime
 
-def funtest(r, c, m, n, l, f, hw, rw, rp):
+def running(test_iteraction, righe, colonne, iteration, drone_number, line_of_sight, loss_factor, has_wall, random_wall,
+                         random_position, alg):
+    media = 0
+    # inizio Test
+    for i in range(test_iteraction):
+        media += funtest(righe, colonne, iteration, drone_number, line_of_sight, loss_factor, has_wall, random_wall,
+                         random_position, alg)  # serve per fare la media
+        print(f"Test {i + 1} di {test_iteraction} completato")
+
+    media /= test_iteraction
+    print(f">> Media: {media} di conoscenza durante i test")
+
+    # JSON
+    # salva i dati dei test
+    risultati = {
+        "algoritmo": "Dijkstra",
+        "righe": righe,
+        "colonne": colonne,
+        "numero droni": drone_number,
+        "numero iterazioni": iteration,
+        "numero di test": iteration,
+        "fattore di dimenticamza": loss_factor,
+        "campo visivo dei droni": line_of_sight,
+        "posizioni dei droni randomiche": random_position,
+        "muri": has_wall,
+        "posizioni muri": random_wall,
+        "conoscenza media globale": media,
+    }
+    # prende il path dei dati
+    file_path = "risultati.json"
+    # aggiunge i dati alla lista
+    if os.path.exists(file_path):
+        with open(file_path, "r") as file:
+            risultati_list = json.load(file)
+    else:
+        risultati_list = []
+    risultati_list.append(risultati)
+    # salva i dati
+    with open(file_path, "w") as file:
+        json.dump(risultati_list, file, indent=4)
+
+
+def funtest(r, c, m, n, l, f, hw, rw, rp, alg):
     griglia = Mappa.MapGrid(r, c, has_wall=hw, random_wall=rw, loss_rate=f)
     for ii in range(0, n):
         drone_i = Drone.Drone(griglia, rand=rp, los=l)
@@ -15,7 +57,7 @@ def funtest(r, c, m, n, l, f, hw, rw, rp):
     print(">>> Inizio simulazione")
     for t in range(m):
         print(f">>>> Turno",t,"di",m)
-        griglia.start(1)
+        griglia.start(alg)
         kn = griglia.print_map_knoledge()
         kn_list.append(kn)
         kn_media += kn
@@ -55,7 +97,7 @@ def funtest(r, c, m, n, l, f, hw, rw, rp):
         json.dump(dati_list, file, indent=4)
     return kn_media
 
-def runnable_show(r, c, m, n, l, f, hw, rw, rp, s):
+def runnable_show(r, c, m, n, l, f, hw, rw, rp, s, alg):
     Config.svuota_cartella('immagini')
 
     griglia = Mappa.MapGrid(r, c, has_wall=hw, random_wall=rw, loss_rate=f)
@@ -71,7 +113,7 @@ def runnable_show(r, c, m, n, l, f, hw, rw, rp, s):
     kn_media = 0
     print(">>> Inizio simulazione")
     for t in range(m):
-        griglia.start(1)
+        griglia.start(alg)
         print(f">>>> Turno {t} di {m}")
         Config.heatmap(griglia, "a", s)  # all
         kn = griglia.print_map_knoledge()
@@ -112,50 +154,313 @@ if __name__ == '__main__':
     has_wall = True # se deve impostare i muri
     random_wall = False # se i muri sono casuali
     random_position = True # se la posizione di partenza dei droni è casuale
+    alg = 1
+
+    #
+    #
+    # TEST CON MURI ALG DIJKSTRA
+    #
+    #
 
 
-    media = 0
 
-    #inizio Test
-    for i in range(test_iteraction):
-        media += funtest(righe, colonne, iteration, drone_number, line_of_sight, loss_factor, has_wall, random_wall,
-                         random_position)  # serve per fare la media
-        print(f"Test {i + 1} di {test_iteraction} completato")
+    # standard test
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
 
-    media /= test_iteraction
-    print(f">> Media: {media} di conoscenza durante i test")
+    # test variando los
+    line_of_sight = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 2
 
-    #JSON
-    # salva i dati dei test
-    risultati = {
-        "algoritmo": "Dijkstra",
-        "righe": righe,
-        "colonne": colonne,
-        "numero droni": drone_number,
-        "numero iterazioni": iteration,
-        "numero di test": iteration,
-        "fattore di dimenticamza": loss_factor,
-        "campo visivo dei droni": line_of_sight,
-        "posizioni dei droni randomiche": random_position,
-        "muri": has_wall,
-        "posizioni muri": random_wall,
-        "conoscenza media globale": media,
-    }
-    # prende il path dei dati
-    file_path = "risultati.json"
-    # aggiunge i dati alla lista
-    if os.path.exists(file_path):
-        with open(file_path, "r") as file:
-            risultati_list = json.load(file)
-    else:
-        risultati_list = []
-    risultati_list.append(risultati)
-    # salva i dati
-    with open(file_path, "w") as file:
-        json.dump(risultati_list, file, indent=4)
+    # Test variando il numero di droni
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test droni e los
+    line_of_sight = 3
+    drone_number = 3
+    # meno droni ma con campo visivo più grande
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test al variare del fattore di dimenticanza
+    line_of_sight = 2
+    drone_number = 4
+    loss_factor = 0.96 # dimenticano di più
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.92
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.90
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.99 # dimenticano di meno
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+
+    # Test misti dimenticano di più ma con piu droni
+    loss_factor = 0.94
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    print("Tutti i test sono stati completati con algoritmo 1")
+
+    #
+    #
+    # TEST SENZA MURI ALG VORONOI
+    #
+    #
+
+    has_wall = False  # se deve impostare i muri
+    random_wall = False  # se i muri sono casuali
+    random_position = True  # se la posizione di partenza dei droni è casuale
+    alg = 0
+
+    # standard test
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # test variando los
+    line_of_sight = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 2
+
+    # Test variando il numero di droni
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test droni e los
+    line_of_sight = 3
+    drone_number = 3
+    # meno droni ma con campo visivo più grande
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test al variare del fattore di dimenticanza
+    line_of_sight = 2
+    drone_number = 4
+    loss_factor = 0.96  # dimenticano di più
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.92
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.90
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.99  # dimenticano di meno
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test misti dimenticano di più ma con piu droni
+    loss_factor = 0.94
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    print("Tutti i test sono stati completati con algoritmo 0")
+
+    #
+    #
+    # TEST SENZA MURI ALG DIJKSTRA
+    #
+    #
+
+    has_wall = False  # se deve impostare i muri
+    random_wall = False  # se i muri sono casuali
+    random_position = True  # se la posizione di partenza dei droni è casuale
+    alg = 1
+
+    # standard test
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # test variando los
+    line_of_sight = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 2
+
+    # Test variando il numero di droni
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test droni e los
+    line_of_sight = 3
+    drone_number = 3
+    # meno droni ma con campo visivo più grande
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    line_of_sight = 4
+    drone_number = 3
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test al variare del fattore di dimenticanza
+    line_of_sight = 2
+    drone_number = 4
+    loss_factor = 0.96  # dimenticano di più
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.92
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.90
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.99  # dimenticano di meno
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+
+    # Test misti dimenticano di più ma con piu droni
+    loss_factor = 0.94
+    drone_number = 6
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.94
+    drone_number = 5
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    loss_factor = 0.96
+    running(test_iteraction, righe, colonne, iteration,
+            drone_number, line_of_sight, loss_factor,
+            has_wall, random_wall, random_position, alg)
+    print("Tutti i test sono stati completati con algoritmo 1")
+
+
+
+
+
+
 
     print(">> Inizia lo show")
     show = True # per far vedere le immagini mentre procede
     # Crea le immagini.
     runnable_show(righe, colonne, iteration, drone_number, line_of_sight, loss_factor, has_wall, random_wall,
-                         random_position, show)
+                         random_position, show, alg)
+
+    # preparazione al Test finale
